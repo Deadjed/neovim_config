72b26e6f421c6c0bca4ad8ee3abdb52087e7a464
Jed Pauckner <64628994+Deadjed@users.noreply.github.com>
2025-04-24 19:29:53 +1000

    Update init.lua to include autocomplete
    Added tabstop/shiftwidth

--- init.lua
+++ init.lua
@@ -26,6 +26,22 @@ require('packer').startup(function(use)
     -- Treesitter syntax highlighting
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
+    -- Autocompletion plugin
+	use {
+	  'hrsh7th/nvim-cmp',
+	  requires = {
+	    'hrsh7th/cmp-nvim-lsp',     -- LSP source for nvim-cmp
+	    'hrsh7th/cmp-buffer',       -- Buffer word completion
+	    'hrsh7th/cmp-path',         -- File path completion
+	    'hrsh7th/cmp-cmdline',      -- Command-line completion
+	    'L3MON4D3/LuaSnip',         -- Snippet engine
+	    'saadparwaiz1/cmp_luasnip', -- Snippet completions
+	  }
+	}
+
+	-- LSP Config
+	use 'neovim/nvim-lspconfig'  -- Collection of configurations for built-in LSP client
+
     -- Optionally add other plugins here
 end)
 
@@ -33,14 +49,49 @@ end)
 vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
 
 -- Set up treesitter
-require('nvim-treesitter.configs').setup {
-    highlight = {
-        enable = true,              -- false will disable the whole extension
-        additional_vim_regex_highlighting = false,
-    },
-    ensure_installed = { "c", "cpp", "python", "prolog" },  -- Specify the languages
+require('nvim-treesitter.configs').setup { 
+	highlight = { 
+		enable = true,              -- false will disable the whole extension 
+		additional_vim_regex_highlighting = true, 
+	}, 
+	ensure_installed = { "cpp", "c", "c_sharp", "python", "prolog"},  -- Specify the languages 
 }
 
--- Set terminal to powershell (for windows only!)
+-- Setup autocomplete (LSP)
+local lspconfig = require('lspconfig')
+
+-- Enable LSP servers here
+-- lspconfig.pyright.setup{}  -- Python
+lspconfig.clangd.setup{}   -- C/C++
+
+-- Set up nvim-cmp
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+
+-- Set shiftwidth/tabstop
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Set terminal
 vim.g.terminal_emulator='powershell'
 vim.opt.shell = 'powershell.exe'
